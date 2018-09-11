75d8309b6958e667517210a44b049b6a16e88a40
Chris Williams <chris.williams@airbnb.com>
2018-09-10 22:03:48 -0700

    [superset-client] initialize SupersetClient in app setup

--- superset/assets/src/common.js
+++ superset/assets/src/common.js
@@ -1,5 +1,6 @@
-/* eslint-disable global-require */
+/* eslint global-require: 0, no-console: 0 */
 import $ from 'jquery';
+import { SupersetClient } from '@superset-ui/core';
 import airbnb from './modules/colorSchemes/airbnb';
 import categoricalSchemes from './modules/colorSchemes/categorical';
 import lyft from './modules/colorSchemes/lyft';
@@ -7,15 +8,14 @@ import { getInstance } from './modules/ColorSchemeManager';
 
 // Everything imported in this file ends up in the common entry file
 // be mindful of double-imports
-
-const utils = require('./modules/utils');
+import { toggleCheckbox } from './modules/utils';
 
 $(document).ready(function () {
   $(':checkbox[data-checkbox-api-prefix]').change(function () {
     const $this = $(this);
     const prefix = $this.data('checkbox-api-prefix');
     const id = $this.attr('id');
-    utils.toggleCheckbox(prefix, '#' + id);
+    toggleCheckbox(prefix, '#' + id);
   });
 
   // for language picker dropdown
@@ -23,10 +23,9 @@ $(document).ready(function () {
     ev.preventDefault();
 
     const targetUrl = ev.currentTarget.href;
-    $.ajax(targetUrl)
-      .then(() => {
-        location.reload();
-      });
+    $.ajax(targetUrl).then(() => {
+      location.reload();
+    });
   });
 });
 
@@ -38,9 +37,15 @@ getInstance()
   .setDefaultSchemeName('bnbColors');
 
 export function appSetup() {
-    // A set of hacks to allow apps to run within a FAB template
+  // A set of hacks to allow apps to run within a FAB template
   // this allows for the server side generated menus to function
   window.$ = $;
   window.jQuery = $;
   require('bootstrap');
+
+  SupersetClient.configure({ host: (window.location && window.location.host) || '' })
+    .init()
+    .catch((error) => {
+      console.warn(error);
+    });
 }
